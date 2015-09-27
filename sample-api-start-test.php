bc65511b4755af9a9f6064d7951ca9bf2f7bb746
Alaeddine Mokri <alaeddinemokri@gmail.com>
2015-09-27 14:44:23 -0700

    added test api page

--- sample-api-start-test.php
+++ sample-api-start-test.php
@@ -1,5 +1,5 @@
 <? include $_SERVER['DOCUMENT_ROOT'].'/header.php'; ?>
 
-<a href="sample-api-start-test.php"><img width="100%" src="images/page-02.jpg"></a>
+<img width="100%" src="images/page-03.jpg">
 
 <? include $_SERVER['DOCUMENT_ROOT'].'/footer.php'; ?>

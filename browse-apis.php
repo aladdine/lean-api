263bfff4ecb6c127b2d577d42bffeb23ebb8eefa
Alaeddine Mokri <alaeddinemokri@gmail.com>
2015-09-27 13:48:14 -0700

    added image

--- browse-apis.php
+++ browse-apis.php
@@ -1,5 +1,5 @@
 <? include $_SERVER['DOCUMENT_ROOT'].'/header.php'; ?>
 
-<h1> add browse apis here </h1>
+<img width="100%" src="page-01.jpg">
 
 <? include $_SERVER['DOCUMENT_ROOT'].'/footer.php'; ?>
